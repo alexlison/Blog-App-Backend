5af0cbabf66ed0aef94033a70f628b4decc2c04a
alexlison <alexlison910@gmail.com>
2026-01-05 14:12:24 +0530

    Remove MongoDB connection string from app.js
    Removed hardcoded MongoDB connection string.

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
 
-mongoose.connect("mongodb+srv://alexlison:alexlison6885@cluster0.bz3d6.mongodb.net/blogDb?retryWrites=true&w=majority&appName=Cluster0")
 
 app.post("/signup", async (req,res) => {
 
